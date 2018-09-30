79ba8f39b2069196d0ef0c4d7ad123d7609ed0be
BS-20180731BRSG\Administrator <wtopull@gmail.com>
2018-09-30 16:37:02 +0800

    sa

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,19 +1,31 @@
+const NODE_ENV = process.env.NODE_ENV
+const isDev = NODE_ENV === 'development'
+const extend = require('./build/webpack.extend')
+const router = require('./build/router')
 module.exports = {
   /* Headers of the page */
   head: {
     title: 'myblog',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge'},
+      { hid: 'description', name: 'description', content: 'Nathan Blog' },
+      { name: 'google', value: 'notranslate'}
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    // script: [{ src: '/iconfont.js', body: true}]  全局iconfont
   },
   /* Customize the progress bar color 自定义进度条颜色 */
   loading: {
-    color: '#F5F5F5'
+    //禁用加载进度条，默认是true
+    // loading: false,
+    //使用自定义加载组件
+    // loading: '~components/loading.vue',
+    color: '#F5F5F5',
+    // failedColor: '页面加载失败时的颜色',
+    // height: '进度条的高度',
+    // duration: "进度条的最大显示时长，单位毫秒。Nuxt.js 假设页面在该时长内加载完毕。",
   },
   css:[
     {
@@ -33,6 +45,22 @@ module.exports = {
           exclude: /(node_modules)/
         })
       }
-    }
+    },
+    filenames: {
+      vendor: 'vendor.[hash].js',
+      app: 'app.[chunkhash].js'
+    },
+    loaders: [{
+      test: /\.(png|jpe?g|gif|svg)$/,
+      loader: 'url-loader',
+      query: {
+        limit: 10000, // 10KO
+        name: 'img/[name].[hash].[ext]'
+      }
+    }],
+    presets: [['vue-app', { targets: isDev ? { chrome: 66} : { ie: 10} }]],
+    vendor: ['axios'],
+    cache: {max: 1000,maxAge: 1000 * 60 * 60},
+    publicPath: '/.nuxt/dist/',
   }
 }
